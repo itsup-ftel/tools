0a6f9952c7a27c66826f072df1072bca551c67e5
it <88810384+itsup-ftel@users.noreply.github.com>
2026-05-07 15:11:59 +0700

    Update it-support.ps1

--- it-support.ps1
+++ it-support.ps1
@@ -28,21 +28,21 @@ echo   %G%======================================================================
 echo                                                 %W%IT SUPPORT PROFESSIONAL TOOLKIT - VERSION 26.5%Res%
 echo   %G%============================================================================================================================================%Res%
 echo.
-echo       %Y%[ 1. HE THONG ]%Res%               %Y%[ 2. PHAN CUNG ]%Res%             %Y%[ 3. MANG ^& INTERNET ]%Res%         %Y%[ 4. MAY IN (PRINTER) ]%Res%
+echo       %Y%[ 1. HE THONG ]%Res%               %Y%[ 2. PHAN CUNG ]%Res%            %Y%[ 3. MANG ^& INTERNET ]%Res%      %Y%[ 4. MAY IN (PRINTER) ]%Res%
 echo.
-echo       %G%01.%Res% Don dep file rac        %G%04.%Res% Kiem tra o cung       %G%12.%Res% Get MAC ^& Serial SN      %G%22.%Res% Restart Print Spooler
-echo       %G%02.%Res% Xem thong so PC         %G%07.%Res% Bao cao suc khoe Pin    %G%13.%Res% Xem chi tiet IP Config   %G%23.%Res% Xoa lenh in bi kiet
-echo       %G%03.%Res% Sua loi SFC ^& DISM      %G%05.%Res% Kiem tra khe RAM   %G%14.%Res% Flush DNS / Renew IP     %G%24.%Res% In thu trang Test Page
-echo       %G%09.%Res% On/Off Windows Update   %G%08.%Res% Xuat Info ra Desktop    %G%15.%Res% Double Ping (GW ^& 8.8)  %G%25.%Res% Liet ke cac may in
-echo       %G%10.%Res% Restart Explorer        %G%26.%Res% Ung dung da cai dat     %G%16.%Res% Xem Pass Wi-Fi cu        %G%--%Res% ----------------------
-echo       %G%18.%Res% Liet ke cac Task        %G%--%Res% ----------------------   %G%17.%Res% Reset thiet lap mang     %G%--%Res% ----------------------
-echo       %G%19.%Res% Dong ung dung treo      %G%--%Res% ----------------------   %G%--%Res% ----------------------   %G%--%Res% ----------------------
-echo       %G%20.%Res% Tat nhanh Chrome        %G%--%Res% ----------------------   %G%--%Res% ----------------------   %G%--%Res% ----------------------
-echo       %G%21.%Res% Danh sach User          %G%--%Res% ----------------------   %G%--%Res% ----------------------   %G%--%Res% ----------------------
+echo       %G%02.%Res% Xem thong so PC         %G%01.%Res% Don dep file rac        %G%12.%Res% Get MAC ^& SN card       %G%22.%Res% Restart Print Spooler
+echo       %G%04.%Res% Kiem tra o cung         %G%03.%Res% Sua loi SFC ^& DISM     %G%13.%Res% Xem chi tiet IP Config   %G%23.%Res% Xoa lenh in bi ket
+echo       %G%21.%Res% Danh sach User          %G%05.%Res% Kiem tra khe RAM        %G%14.%Res% Flush DNS / Renew IP     %G%24.%Res% In thu trang Test Page
+echo       %G%26.%Res% Ung dung da cai dat     %G%09.%Res% On/Off Windows Update   %G%15.%Res% Double Ping (GW ^& 8.8)  %G%25.%Res% Liet ke d/s may in
+echo       %G%--%Res% ----------------------   %G%10.%Res% Restart Explorer        %G%16.%Res% Xem Pass Wi-Fi cu        %G%--%Res% ----------------------
+echo       %G%--%Res% ----------------------   %G%18.%Res% Liet ke cac Task        %G%17.%Res% Reset thiet lap mang     %G%--%Res% ----------------------
+echo       %G%--%Res% ----------------------   %G%19.%Res% Dong ung dung treo      %G%--%Res% ----------------------    %G%--%Res% ----------------------
+echo       %G%--%Res% ----------------------   %G%20.%Res% Tat nhanh Chrome        %G%--%Res% ----------------------    %G%--%Res% ----------------------
+echo       %G%--%Res% ----------------------   %G%--%Res% ----------------------   %G%--%Res% ----------------------    %G%--%Res% ----------------------
 echo.
-echo       %C%[ 5. TRUY CAP NHANH 1 ]%Res%       %C%[ 6. TRUY CAP NHANH 2 ]%Res%       %C%[ 7. CAI DAT - KICH HOAT ]%Res%              %C%[ 8. LENH NHANH ]%Res%
+echo       %C%[ 5. TRUY CAP NHANH 1 ]%Res%       %C%[ 6. TRUY CAP NHANH 2 ]%Res%     %C%[ 7. CAI DAT - KICH HOAT ]%Res%    %C%[ 8. LENH NHANH ]%Res%
 echo.
-echo       %G%31.%Res% Control Panel           %G%35.%Res% Print Management        %G%39.%Res% Cai dat OFFICE          %G%--%Res% ----------------------
+echo       %G%31.%Res% Control Panel           %G%35.%Res% Print Management        %G%39.%Res% Cai dat OFFICE           %G%--%Res% ----------------------
 echo       %G%32.%Res% Task Manager            %G%36.%Res% Network Connections     %G%40.%Res% %G%Active WIN/OFFICE%Res%  %G%--%Res% ----------------------
 echo       %G%33.%Res% Services (msc)          %G%37.%Res% Registry Editor         %G%--%Res% ----------------------   %G%--%Res% ----------------------
 echo       %G%34.%Res% Device Manager          %G%38.%Res% Advanced Firewall       %G%--%Res% ----------------------   %G%--%Res% ----------------------
