d4ea3d56e997a99411260b23103f57f31cb6a8ec
it <88810384+itsup-ftel@users.noreply.github.com>
2026-05-10 09:57:50 +0700

    Update Acrobatpro.ps1

--- file/Acrobatpro.ps1
+++ file/Acrobatpro.ps1
@@ -1,20 +1,20 @@
-# Guide available at https://www.reddit.com/r/GenP/
+# Active Adobe Acrobat DC Pro
 
 $ErrorActionPreference = "Stop"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/GenP-V/Acropolis/main/Acropolis.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/itsup-ftel/tools/refs/heads/main/file/Acrobatpro.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\Acropolis_$rand.cmd" } else { "$env:TEMP\Acropolis_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\Acrobatpro_$rand.cmd" } else { "$env:TEMP\Acrobatpro_$rand.cmd" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
 }
 catch {
-    Write-Error "Failed to download Acropolis.cmd from $DownloadURL"
+    Write-Error "Failed to download Acrobatpro.cmd from $DownloadURL"
     exit 1
 }
 
@@ -25,5 +25,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\Acropolis*.cmd", "$env:SystemRoot\Temp\Acropolis*.cmd")
+$FilePaths = @("$env:TEMP\Acrobatpro*.cmd", "$env:SystemRoot\Temp\Acrobatpro*.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
