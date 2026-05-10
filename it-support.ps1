00b8b8ecac3c60cc20860d6a51b25afae70c208b
it <88810384+itsup-ftel@users.noreply.github.com>
2026-05-10 10:26:49 +0700

    Update it-support.ps1

--- it-support.ps1
+++ it-support.ps1
@@ -269,7 +269,7 @@ echo %C%Luu y: May tinh can co ket noi Internet.%Res%
 echo.
 :: Sua loi: Them goi powershell de CMD co the thuc thi lenh irm
 :: powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd | iex"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$url = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd'; $file = \"$env:TEMP\MAS_AIO.cmd\"; iwr $url -OutFile $file; & $file"
 goto menu
 
 :acrobat
