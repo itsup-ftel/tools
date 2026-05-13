42b6b5179742ebe36b690f2e1a8c1dd302061e38
it <88810384+itsup-ftel@users.noreply.github.com>
2026-05-13 16:01:05 +0700

    Update it-support.ps1

--- it-support.ps1
+++ it-support.ps1
@@ -315,9 +315,9 @@ echo:
 echo:                    %C%[Foxit PDF Editor x2024]%Res%
 echo:     ______________________________________________________________
 echo:         [1] %G%FULL%Res%: Tai, Cai dat ^& Kich hoat
-echo:         [2] Chi kich hoat Editor (Neu da cai san app)
+echo:         [2] Chi kich hoat Editor %Y%(Neu da cai san app)%Res%
 echo:         [3] Chan Firewall (Chan quet ban quyen)
-echo:         [0] Thoat ve menu chinh
+echo:         [0] %R%Thoat ve menu chinh%Res%
 echo:     ______________________________________________________________
 echo.
 choice /C:1230 /N
