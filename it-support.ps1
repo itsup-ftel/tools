ea38e95f009577f68b159c0c4af8b7490e594e21
it <88810384+itsup-ftel@users.noreply.github.com>
2026-05-26 14:21:50 +0700

    Update it-support.ps1

--- it-support.ps1
+++ it-support.ps1
@@ -76,7 +76,7 @@ echo      %G%4.%Res% Thong tin User       %G%10.%Res% On/Off Win Update   %G%16.
 echo      %G%5.%Res% Thong tin Bitlocker  %G%11.%Res% Restart Explorer    %G%17.%Res% Xem Pass Wi-Fi       %G%23.%Res% Them may in moi
 echo      %G%6.%Res% Thong tin Ban quyen  %G%12.%Res% Xu ly Task          %G%18.%Res% Reset Mang           %G%24.%Res% Xu ly loi may in
 echo.
-echo     %C%[ 5. CONG CU 1 ]%Res%        %C%[ 6. CONG CU 2-ado ]%Res%         %C%[ 7. CAI DAT ]%Res%           %C%[ 8. FIX LOI ]%Res%
+echo     %C%[ 5. CONG CU 1 ]%Res%        %C%[ 6. CONG CU 2-3ds ]%Res%         %C%[ 7. CAI DAT ]%Res%           %C%[ 8. FIX LOI ]%Res%
 echo.
 echo     %G%25.%Res% Control Panel        %G%30.%Res% Print Management    %G%35.%Res% Bo cai Office       %G%40.%Res% Sao luu/ Phuc hoi
 echo     %G%26.%Res% Task Manager         %G%31.%Res% Network Connection  %G%36.%Res% %G%Active Win/Office%Res%   %G%41.%Res% Dich vu cong
@@ -748,14 +748,14 @@ echo 5. Thoat ve Menu chinh
 echo %C%==================================================%Res%
 set /p choice="Chon lua chon cua ban (1-5): "
 
-if "%choice%"=="1" goto :status
+if "%choice%"=="1" goto :bitlockerstatus
 if "%choice%"=="2" goto :disable_one
 if "%choice%"=="3" goto :disable_all
 if "%choice%"=="4" goto :monitor
 if "%choice%"=="5" goto menu
-goto :bitlocker
+goto :menu
 
-:status
+:bitlockerstatus
 echo.
 echo %C%[--- TRANG THAI BITLOCKER ---]%Res%
 powershell -command "Get-BitLockerVolume | Select-Object MountPoint, VolumeStatus, ProtectionStatus, EncryptionPercentage | Format-Table -AutoSize"
