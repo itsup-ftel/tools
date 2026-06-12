e1cd07a0e6b56de3229727abb04a37ee3cd82c82
it <88810384+itsup-ftel@users.noreply.github.com>
2026-06-12 21:32:46 +0700

    Update it-support.ps1

--- it-support.ps1
+++ it-support.ps1
@@ -67,7 +67,7 @@ echo  %G%=======================================================================
 echo                                     %W%IT SUPPORT PROFESSIONAL TOOLKIT - VERSION 26.5%Res%
 echo  %G%====================================================================================================================%Res%
 echo.
-echo     %Y%[ 1. KIEM TRA]%Res%           %Y%[ 2. HE THONG ]%Res%         %Y%[ 3. NETWORK ]%Res%          %Y%[ 4. MAY IN ]%Res%
+echo     %Y%[ 1. KIEM TRA]%Res%           %Y%[ 2. HE THONG ]%Res%         %Y%[ 3. NETWORK ]%Res%           %Y%[ 4. MAY IN ]%Res%
 echo.
 echo      %G%1.%Res% Thong so may PC       %G%7.%Res% Don dep rac         %G%13.%Res% Lay MAC ^& Serial     %G%19.%Res% Restart Spooler
 echo      %G%2.%Res% Thong tin O cung      %G%8.%Res% Sua loi SFC/DISM    %G%14.%Res% Cau hinh IP/DNS      %G%20.%Res% Xoa ket lenh in
@@ -76,7 +76,7 @@ echo      %G%4.%Res% Thong tin User       %G%10.%Res% On/Off Win Update   %G%16.
 echo      %G%5.%Res% Thong tin Bitlocker  %G%11.%Res% Restart Explorer    %G%17.%Res% Xem Pass Wi-Fi       %G%23.%Res% Quan ly may in
 echo      %G%6.%Res% Thong tin Ban quyen  %G%12.%Res% Xu ly Task          %G%18.%Res% Reset Mang           %G%24.%Res% Xu ly loi may in
 echo.
-echo     %C%[ 5. CONG CU 1 ]%Res%         %C%[ 6. CONG CU 2 ]%Res%       %C%[ 7. CAI DAT 1]%Res%          %C%[ 8. CAI DAT 2 ]%Res%
+echo     %C%[ 5. CONG CU 1 ]%Res%         %C%[ 6. CONG CU 2 ]%Res%        %C%[ 7. CAI DAT 1]%Res%         %C%[ 8. CAI DAT 2 ]%Res%
 echo.
 echo     %G%25.%Res% Control Panel        %G%30.%Res% Print Management    %G%35.%Res% Bo cai Office       %G%40.%Res% Sao luu/ Phuc hoi
 echo     %G%26.%Res% Task Manager         %G%31.%Res% Network Connection  %G%36.%Res% %G%Active Win/Office%Res%   %G%41.%Res% Dich vu cong
